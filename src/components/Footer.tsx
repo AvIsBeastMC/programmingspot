150798c86ca9c4d8f0e0505f2ef1729b22911f6b
coolcoder286 <ansh.shawrikar.4@gmail.com>
2022-11-13 19:41:25 +0530

    added links to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
             © {new Date().getFullYear()} Programming Spot
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a className="text-gray-black">
+            <a className="text-gray-black" href="https://www.facebook.com/profile.php?id=100087250854360">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -24,7 +24,7 @@ const Footer: React.FC = () => {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-black">
+            <a className="ml-3 text-gray-black" href="https://twitter.com/programingspot">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
@@ -36,7 +36,7 @@ const Footer: React.FC = () => {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-black">
+            <a className="ml-3 text-gray-black" href="https://www.instagram.com/spotprogramming/">
               <svg
                 fill="none"
                 stroke="currentColor"
